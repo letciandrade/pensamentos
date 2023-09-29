0c6054ab4dcbb545e94f535537c54eabc46c9ffd
letciandrade <leticia.andrade17020@gmailcom>
2023-09-29 14:42:41 -0300

    configurado

--- index.js
+++ index.js
@@ -3,5 +3,62 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const FileStore =  require('session-file-store')(session)
 const flash = require('express-flash')
+
 const app = express()
+
 const conn = require('./db/conn')
+
+//importar models
+
+//importar rotas
+
+//importar controleer
+
+//config engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//config json
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+//config middleware para as sessoes
+app.use(session({
+  name:'session',
+  secret:'nosso_secret', //quanto maior a crypto melhor rsrs
+  resave:false,
+  saveUninitialized:false,
+  store: new FileStore({
+    logFn: function (){},
+    path: require('path').join(require('os').tmpdir(),'sessions') // gambiarra.
+  }),
+  cookie:{
+    secure:false,
+    maxAge: 360000,
+    expires: new Date(Date.now() + 360000),
+    httpOnly:true
+  }
+
+
+}))
+//importar as flashs
+app.use(flash())
+
+//importar arquivos estáticos
+app.use(express.static('public'))
+
+//armazenar as sessoes nas rotas
+app.use((req,res,next)=>{
+  if(req.session.userId){
+    res.locals.session = req.session
+  }
+  next()
+})
+//rotas
+
+//conexao e criação das tabelas do banco
+conn
+.sync()
+.then(()=>{
+  app.listen(2333)
+}).catch((err)=> console.log(err))
